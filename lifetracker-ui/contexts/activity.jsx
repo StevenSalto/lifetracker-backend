5bfc3ad323a278f9537680a152fec3ae6fe22851
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-07-09 11:33:30 -0400

    Delete lifetracker-ui directory

--- lifetracker-ui/contexts/activity.jsx
+++
@@ -1,3 +0,0 @@
-import * as React from 'react'
-
-var ActivityContext = new React.createContext;
--- lifetracker-ui/src/components/NotFound/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-    return (
-        <div className="not-found">
-            <h1>NOT FOUND</h1>
-            <p>Whatever you're looking for ain't here pal.</p>
-
-        </div>
-    )
-}
